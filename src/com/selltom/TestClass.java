1af66ee4872410f58e3c62eeaf5b9798e7f71670
Sellto <Impossible1985>
2017-10-26 10:55:39 +0200

    Fix Junit

---
+++ src/com/selltom/TestClass.java
@@ -0,0 +1,12 @@
+package com.selltom;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class TestClass {
+    @Test
+    void arrondiTest() {
+        assertEquals(1,1);
+    }
+}
